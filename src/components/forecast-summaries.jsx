3ca30880527a1c81a4278a7d98fa4f59abd9a972
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-08-20 14:57:04 +0100

    weather application updates

---
+++ src/components/forecast-summaries.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ForecastSummary from './forecast-summary';
+import PropTypes from 'prop-types';
+import LocationDetails from './location-details';
+
+const App = props => (
+  <div className="forecast">
+    <LocationDetails
+      city={props.location.city}
+      country={props.location.country}
+    />
+    <ForecastSummaries forecasts={props.forecast} />
+  </div>
+);
+
+const ForecastSummaries = props => (
+  <div className="forecast-summaries">
+    {
+      props.forecasts.map(forecast => (
+        <ForecastSummary
+          key={forecast.date}
+          date={forecast.date}
+          description={forecast.description}
+          icon={forecast.icon}
+          temperature={forecast.temperature.max}
+        />
+      ))
+    }
+  </div>
+
+ForecastSummary.propTypes = {
+  date: PropTypes.number.isRequired,
+  temperature: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequ
+};
+
+export default ForecastSummaries;
